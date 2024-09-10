0e21306d9aa0e358ffe3947659b46286900eb772
Sam Ahrens <339729139@GoTVDSB.ca>
2024-09-10 09:38:50 -0400

    Add project files.

---
+++ Topic 3 - Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Topic_3___Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sam Ahrens 09/10/24
+            int roomNumber = 29;
+            double price = 2.99;
+            string course = "Computer Science";
+
+            Console.WriteLine("Welcome to room #" + (roomNumber) + "!");
+            Console.WriteLine();
+            Console.WriteLine("The price is :$" + (price));
+            Console.WriteLine();
+            Console.WriteLine("I am learning a bit about " + (course) + "!");
+        }
+    }
+}
